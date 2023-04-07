65fc31d61344eebfdf4ad73d14668fe91fc587e3
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-04-07 22:28:36 +0200

    Add ini config file support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::path::Path;
+
+use configparser::ini::Ini;
 use glow::HasContext;
 use imgui::Context;
 use imgui_glow_renderer::AutoRenderer;
@@ -10,13 +14,6 @@ use sdl2::{
     video::{GLProfile, Window},
 };
 use simple_logger::SimpleLogger;
-
-// Create a new glow context.
-fn glow_context(window: &Window) -> glow::Context {
-    unsafe {
-        glow::Context::from_loader_function(|s| window.subsystem().gl_get_proc_address(s) as _)
-    }
-}
 
 fn init_logging() {
     SimpleLogger::new().init().unwrap();
@@ -41,24 +38,39 @@ fn init_window(sdl_video: &VideoSubsystem, title: &str, width: u32, height: u32)
     return window;
 }
 
-fn init_gl(window: &Window) -> GLContext {
+fn init_opengl(window: &Window) -> GLContext {
     let gl_context = window.gl_create_context().unwrap();
     window.gl_make_current(&gl_context).unwrap();
     window.subsystem().gl_set_swap_interval(1).unwrap();
     return gl_context;
 }
 
+fn init_glow_context(window: &Window) -> glow::Context {
+    unsafe {
+        glow::Context::from_loader_function(|s| window.subsystem().gl_get_proc_address(s) as _)
+    }
+}
+
 fn main() {
     /* Initialize logging */
     init_logging();
+
+    /* Initialize configuration */
+    let mut ini = Ini::new();
+    let config = if Path::new("./config.ini").exists() {
+        ini.load("config.ini").unwrap()
+    } else {
+        HashMap::default()
+    };
+    println!("{:?}", config);
 
     /* Initialize SDL */
     let sdl = sdl2::init().unwrap();
     let sdl_video = sdl.video().unwrap();
     init_video(&sdl_video);
     let window = init_window(&sdl_video, "Base Project", 800, 600);
-    let _gl_context = init_gl(&window);
-    let glow_context = glow_context(&window);
+    let _gl_context = init_opengl(&window); // closes on drop
+    let glow_context = init_glow_context(&window);
 
     /* Initialize ImGui */
     let mut imgui_context = Context::create();
@@ -70,9 +82,7 @@ fn main() {
     let mut imgui_platform = SdlPlatform::init(&mut imgui_context);
     let mut imgui_renderer = AutoRenderer::initialize(glow_context, &mut imgui_context).unwrap();
 
-    /* start main loop */
     let mut event_pump = sdl.event_pump().unwrap();
-
     'main_loop: loop {
         for event in event_pump.poll_iter() {
             imgui_platform.handle_event(&mut imgui_context, &event);
@@ -89,7 +99,7 @@ fn main() {
             }
         }
 
-        /* Update ImGui UI */
+        /* ImGui update */
         imgui_platform.prepare_frame(&mut imgui_context, &window, &event_pump);
         let ui = imgui_context.new_frame();
         if let Some(window) = ui.window("Example Window").begin() {
