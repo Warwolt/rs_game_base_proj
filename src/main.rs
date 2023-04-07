97801b6cb1bbdf984104c3f5b7719af758237b24
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-04-07 21:49:37 +0200

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use imgui::Context;
 use imgui_glow_renderer::AutoRenderer;
 use imgui_sdl2_support::SdlPlatform;
 use sdl2::keyboard::Keycode;
+use sdl2::video::GLContext;
+use sdl2::VideoSubsystem;
 use sdl2::{
     event::Event,
     video::{GLProfile, Window},
@@ -16,23 +18,19 @@ fn glow_context(window: &Window) -> glow::Context {
     }
 }
 
-fn main() {
+fn init_logging() {
     SimpleLogger::new().init().unwrap();
-    log::info!("hello world");
+}
 
-    /* initialize SDL and its video subsystem */
-    let sdl = sdl2::init().unwrap();
-    let video_subsystem = sdl.video().unwrap();
-
-    /* hint SDL to initialize an OpenGL 3.3 core profile context */
-    let gl_attr = video_subsystem.gl_attr();
-
+fn init_video(sdl_video: &VideoSubsystem) {
+    let gl_attr = sdl_video.gl_attr();
     gl_attr.set_context_version(3, 3);
     gl_attr.set_context_profile(GLProfile::Core);
+}
 
-    /* create a new window, be sure to call opengl method on the builder when using glow! */
-    let window = video_subsystem
-        .window("Hello imgui-rs!", 1280, 720)
+fn init_window(sdl_video: &VideoSubsystem, title: &str, width: u32, height: u32) -> Window {
+    let window = sdl_video
+        .window(title, width, height)
         .allow_highdpi()
         .opengl()
         .position_centered()
@@ -40,44 +38,50 @@ fn main() {
         .build()
         .unwrap();
 
-    /* create a new OpenGL context and make it current */
+    return window;
+}
+
+fn init_gl(window: &Window) -> GLContext {
     let gl_context = window.gl_create_context().unwrap();
     window.gl_make_current(&gl_context).unwrap();
-
-    /* enable vsync to cap framerate */
     window.subsystem().gl_set_swap_interval(1).unwrap();
+    return gl_context;
+}
 
-    /* create new glow and imgui contexts */
-    let gl = glow_context(&window);
+fn main() {
+    /* Initialize logging */
+    init_logging();
 
-    /* create context */
-    let mut imgui = Context::create();
+    /* Initialize SDL */
+    let sdl = sdl2::init().unwrap();
+    let sdl_video = sdl.video().unwrap();
+    init_video(&sdl_video);
+    let window = init_window(&sdl_video, "Base Project", 800, 600);
+    let _gl_context = init_gl(&window);
+    let glow_context = glow_context(&window);
 
-    /* disable creation of files on disc */
-    imgui.set_ini_filename(None);
-    imgui.set_log_filename(None);
-
-    /* setup platform and renderer, and fonts to imgui */
-    imgui
+    /* Initialize ImGui */
+    let mut imgui_context = Context::create();
+    imgui_context.set_ini_filename(None);
+    imgui_context.set_log_filename(None);
+    imgui_context
         .fonts()
         .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
-
-    /* create platform and renderer */
-    let mut imgui_backend = SdlPlatform::init(&mut imgui);
-    let mut renderer = AutoRenderer::initialize(gl, &mut imgui).unwrap();
+    let mut imgui_platform = SdlPlatform::init(&mut imgui_context);
+    let mut imgui_renderer = AutoRenderer::initialize(glow_context, &mut imgui_context).unwrap();
 
     /* start main loop */
     let mut event_pump = sdl.event_pump().unwrap();
 
-    'main: loop {
+    'main_loop: loop {
         for event in event_pump.poll_iter() {
-            imgui_backend.handle_event(&mut imgui, &event);
+            imgui_platform.handle_event(&mut imgui_context, &event);
 
             match event {
-                Event::Quit { .. } => break 'main,
+                Event::Quit { .. } => break 'main_loop,
                 Event::KeyDown { keycode, .. } => match keycode {
                     Some(Keycode::Escape) => {
-                        break 'main;
+                        break 'main_loop;
                     }
                     _ => {}
                 },
@@ -85,21 +89,18 @@ fn main() {
             }
         }
 
-        /* call prepare_frame before calling imgui.new_frame() */
-        imgui_backend.prepare_frame(&mut imgui, &window, &event_pump);
-        let ui = imgui.new_frame();
-
-        /* create imgui UI here */
+        /* Update ImGui UI */
+        imgui_platform.prepare_frame(&mut imgui_context, &window, &event_pump);
+        let ui = imgui_context.new_frame();
         if let Some(window) = ui.window("Example Window").begin() {
             ui.text("Window is visible");
             window.end();
         };
 
         /* render */
-        let draw_data = imgui.render();
-
-        unsafe { renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
-        renderer.render(draw_data).unwrap();
+        let imgui_draw_data = imgui_context.render();
+        unsafe { imgui_renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
+        imgui_renderer.render(imgui_draw_data).unwrap();
 
         window.gl_swap_window();
     }
